503829d45e3dc7a1e8f66fa5eb0bc3fd7cb25a68
PraneethVithanage <37531109+PraneethVithanage@users.noreply.github.com>
2020-04-18 19:13:10 +0530

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by praneeth vithanage on 4/18/20.
+//  Copyright © 2020 praneeth vithanage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
